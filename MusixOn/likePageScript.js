89f2953d42bbfaeffc4d83f4df84131610cebbdf
hvsrathore10 <harshvardhan8991@gmail.com>
2023-07-21 14:35:32 +0530

    Fixed Some error

--- MusixOn/likePageScript.js
+++ MusixOn/likePageScript.js
@@ -7,3 +7,25 @@ const PlaylistId = document.getElementById('PlaylistId');
 playlistFunc.addEventListener('click',() => {
     PlaylistId.style.display = "block";
 });
+
+function toggleSearchBox() {
+    var searchIcon = document.getElementById("search-icon");
+    var searchContainer = document.getElementById("search-container");
+  
+    if (searchIcon.classList.contains("fa-magnifying-glass")) {
+      searchIcon.classList.remove("fa-magnifying-glass");
+      searchIcon.classList.add("fa-times");
+  
+      var searchBox = document.createElement("input");
+      searchBox.setAttribute("type", "text");
+      searchBox.setAttribute("placeholder", "Search");
+      searchContainer.appendChild(searchBox);
+    } else {
+      searchIcon.classList.remove("fa-times");
+      searchIcon.classList.add("fa-magnifying-glass");
+  
+      var searchBox = document.querySelector("#search-container input");
+      searchContainer.removeChild(searchBox);
+    }
+  }
+  
